14086df62875f74d5b1a2e1ba66c8900cd35993f
engabdulrhman5 <abdulrhmane735@gmail.com>
2026-09-11 23:42:23 +0300

    feat: make balance formatter respect selected currency

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,39 +1,23 @@
+const { formatCurrency } = require("../services/currencyService");
+
 function escapeHtml(value) {
-  return String(value)
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#39;");
+  return String(value).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;");
 }
 
-function formatRuble(amount) {
-  return Number(amount || 0).toFixed(2).replace(/\.00$/, "");
+function formatRuble(amount, currency) {
+  const selected = currency || global.__VAULTX_ACTIVE_CURRENCY || "RUB";
+  return formatCurrency(amount, selected);
 }
 
 function getDisplayName(user) {
-  if (user.firstName) {
-    return user.firstName;
-  }
-
-  if (user.username) {
-    return `@${user.username}`;
-  }
-
+  if (user.firstName) return user.firstName;
+  if (user.username) return `@${user.username}`;
   return "غير محدد";
 }
 
 function getDisplayUsername(user) {
-  if (user.username) {
-    return `@${user.username}`;
-  }
-
+  if (user.username) return `@${user.username}`;
   return "غير محدد";
 }
 
-module.exports = {
-  escapeHtml,
-  formatRuble,
-  getDisplayName,
-  getDisplayUsername,
-};
+module.exports = { escapeHtml, formatRuble, getDisplayName, getDisplayUsername };
